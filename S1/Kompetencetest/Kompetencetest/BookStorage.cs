19e2eb9254c41ba10e5bb72ccf6488e09e68f902
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-05-15 10:07:26 +0200

    Lavede en comment

--- S1/Kompetencetest/Kompetencetest/BookStorage.cs
+++ S1/Kompetencetest/Kompetencetest/BookStorage.cs
@@ -24,7 +24,7 @@ namespace BookManagerProject
                 Book book = Books[i];
                 sb.AppendLine(string.Format("{0}|{1}|{2}|{3}|{4}", book.Title, book.ISBN, book.Author, book.Amount, book.IsPaperback));
             }
-            File.WriteAllText("Books.txt", sb.ToString());
+            File.WriteAllText("Books.txt", sb.ToString()); // Laver en ny text fil, eller overskriver den hvis den allerede existere, med den nye text
         }
 
         public static void LoadBooks()
