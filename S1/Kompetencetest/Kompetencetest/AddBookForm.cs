17fc54645da76c1c5f9c8c8fa806ebc02f4014bb
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-05-15 10:16:06 +0200

    Re-uploaded project

---
+++ S1/Kompetencetest/Kompetencetest/AddBookForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManagerProject
+{
+    public partial class AddBookForm : Form
+    {
+        public AddBookForm()
+        {
+            InitializeComponent();
+        }
+
+        private void doneBtn_Click(object sender, EventArgs e)
+        {
+            if(InformationFilled())
+            {
+                Form1 f1 = Owner as Form1;
+                f1.AddBook(new Book(titleTB.Text, isbnTB.Text, authorTB.Text, (int)amountNum.Value, isPaperbackCB.Checked), true);
+                Close();
+            }
+        }
+
+        private bool InformationFilled()
+        {
+            return titleTB.Text != string.Empty && isbnTB.Text != string.Empty && authorTB.Text != string.Empty;
+        }
+    }
+}
---
+++ S1/Kompetencetest/Kompetencetest/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagerProject
+{
+    public class Book
+    {
+        public string Title { get; private set; }
+        public string ISBN { get; private set; }
+        public string Author { get; private set; }
+        public int Amount { get; private set; }
+        public bool IsPaperback { get; private set; }
+
+        public Book(string title, string isbn, string author, int amount, bool isPaperback)
+        {
+            Title = title;
+            ISBN = isbn;
+            Author = author;
+            Amount = amount;
+            IsPaperback = isPaperback;
+        }
+    }
+}
